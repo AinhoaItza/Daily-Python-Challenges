3a888e64215a78d64c0c8539a76a605e3cdeef3f
Ainhoa Itza <u2571108@unimail.hud.ac.uk>
2026-02-21 23:58:53 +0000

    refactor: update IfStatements.py with improved logic and comments; remove prueba.py

--- challenges-012-019-if-statements/IfStatements.py
+++ challenges-012-019-if-statements/IfStatements.py
@@ -1,22 +1,40 @@
-#Teoria
-num1=20
-if num1 > 10:
-    print("This is over 10")
-elif num1 == 10:
-    print("This is equals to 10")
+# #Teoria
+# num1 = 20
+# if num1 > 10:
+#     print("This is over 10")
+# elif num1 == 10:
+#     print("This is equals to 10")
+# else:
+#     print("This is under 10")
+
+# num = int(input("Enter a number between 10 and 20: "))
+# if num>= 10 and num <= 20:
+#     print("Thank you")
+# else:
+#     print("Out of range")
+# #aqui se usa "and", means que tienen que ser ciertas para imprimir
+
+# num = int(input("Enter an EVEN number between 1 and 5: "))
+# if num == 2 or num == 4:
+#     print("Thank you")
+# else:
+#     print("Incorrect")
+# #aqui se usa "or", means que al menos tiene que se un numero par
+# para que se cumpla
+
+num = 30
+if num >= 10:
+    if num <= 20:
+        print("This is between 10 and 20")
+    else:
+        print("This is over 20")
 else:
     print("This is under 10")
 
-num=int(input("Enter a number between 10 and 20: "))
-if num>= 10 and num <= 20:
-    print("Thank you")
-else:
-    print("Out of range")
-#aqui se usa "and", means que si o si tienen que ser ciertas para imprimir el thank you
+text = str.lower(text) #Changes the text to lower case.
 
-num=int(input("Enter an EVEN number between 1 and 5: "))
-if num == 2 or num == 4:
+num = int(input("Enter a number between 10 and 20: "))
+if num >= 10 and num <= 20:
     print("Thank you")
 else:
-    print("Incorrect")
-#aqui se usa "or", means que al menos tiene que se un numero par (2 o 4) para que se cumpla
+    print("Out of range")    
--- prueba.py
+++
@@ -1,2 +0,0 @@
-print("Hola, mundo!") # Este es un comentario en Python.
-#print("¡Bienvenido a la programación en Python!") # La línea anterior está comentada y no se ejecutará.
